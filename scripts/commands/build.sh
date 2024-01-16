ec017104eb69cf3403f43de44c1eb6a923262385
Dmitry Zhadinets <dzhadinets@gmail.com>
2024-01-16 00:49:41 -0600

    * Fix rootfs launch rdinit is set to /sbin/init since it is my init and nothing else Other fixes are tuning and cleaning

--- scripts/commands/build.sh
+++ scripts/commands/build.sh
@@ -74,7 +74,7 @@ function build_initrd
 {
     log_info "Building InitRamDisk at $YALDA_INITRD"
     pushd "$YALDA_OUTPUT_DIR" >/dev/null
-    find . -print0 | cpio --null -ov --format=newc 2>/dev/null | gzip -9 > "$YALDA_INITRD"
+    find . -print0 | cpio --null -ov --format=newc 2>/dev/null | gzip > "$YALDA_INITRD"
     popd >/dev/null
 }
 
