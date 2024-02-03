b62dc631b531a9d10b936d631175e4c34406b6b2
Dmitry Zhadinets <dzhadinets@gmail.com>
2024-02-03 10:41:28 -0600

    + Added option to format defined set of files but not all

--- scripts/commands/code-format.sh
+++ scripts/commands/code-format.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-find . -iname *.h -o -iname *.c | grep -vF '.yalda' | xargs clang-format -i -style=file:$YALDA_KERNEL_SOURCE_DIR/.clang-format
+if [ -n $1 ];then
+    echo $@  | xargs clang-format -i -style=file:$YALDA_KERNEL_SOURCE_DIR/.clang-format
+else
+    find . -iname *.h -o -iname *.c | grep -vF '.yalda' | xargs clang-format -i -style=file:$YALDA_KERNEL_SOURCE_DIR/.clang-format
+fi
